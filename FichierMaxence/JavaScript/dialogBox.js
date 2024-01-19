8abd94e8c06b1014e8980158f10519243f8d3196
GeekTagArmes05 <53153546+GeekTagArmes05@users.noreply.github.com>
2024-01-19 01:05:41 +0100

    Update dialogBox.js

--- FichierMaxence/JavaScript/dialogBox.js
+++ FichierMaxence/JavaScript/dialogBox.js
@@ -1,3 +1,5 @@
+// Cree par EUSOP Maxence 18/01/2024
+
 var tableauDialog = [];
 
 tableauDialog[0] = "Bienvenue sur Escape the redoublement ! Pour commencer le jeu, appuie sur le tram pour arriver jusqu'au batiment. Good luck !";
