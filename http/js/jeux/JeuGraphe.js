9067c2426b80b6c5a962e047a89730fa5940a41f
Maxence <maxeusop@gmail.com>
2024-01-24 16:31:08 +0100

    MAJ du jeuGraphe

--- http/js/jeux/JeuGraphe.js
+++ http/js/jeux/JeuGraphe.js
@@ -18,14 +18,6 @@ function reponseChecked(radioName){
 
 // -----------------------------------------------------------------------------
 
-// Fonction qui cache le jeu après la réussite :
-function after(){
-    document.querySelector(".boxOverall").style.display = "none";
-    document.querySelector("img").style.filter ="none";
-}
-
-// -----------------------------------------------------------------------------
-
 // Fonction qui ajoute une couleur en fonction de la réponse du joueur, de plus en cas de victoire le jeu s'arrete :
 function verificationReponse(){
     var questions = [reponseChecked("Reponse1"), reponseChecked("Reponse2"), reponseChecked("Reponse3"), reponseChecked("Reponse4")];
@@ -62,8 +54,8 @@ function verificationReponse(){
     compteur = compteur + 1;
     if(questions[0] == 'true' && questions[1] == 'true' && questions[2] == 'true' && questions[3] == 'true'){
         alert("Bien joué tu as répondu correctement à toutes les questions en " + compteur + " essais !");
-        after();
         localStorage.setItem("jeuGraphe", "true");
+        window.location.href = "../pages/jeu.html";
     }
     else{
         alert("Recommence");
