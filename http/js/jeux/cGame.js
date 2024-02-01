f71fe1c35135b08d7dc8954061371af30775ce0b
MatTayzer <matthieu.cartiere1211@gmail.com>
2024-02-01 15:07:30 +0100

    Début jeu de C

---
+++ http/js/jeux/cGame.js
@@ -0,0 +1,14 @@
+var answer = [
+    "float",
+    "free()",
+    "float a = 2.8;",
+    "int *pointeurNombr = &nombre;",
+    "curled up",
+    "prepared",
+    "finished",
+    "completed",
+    "blossom",
+    "reads",
+    "prefer",
+    "and"];
+var result = 0;
