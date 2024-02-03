d6e3c149f73c2e8817b1152f0b7515cae4d6b0e2
EstebanDEG <124689055+EstebanDEG@users.noreply.github.com>
2024-02-03 14:51:04 +0100

    Preparation des zones cliquables et modifications des maps

--- http/js/scene-swap.js
+++ http/js/scene-swap.js
@@ -3,71 +3,58 @@
 function replaceImage(target) {
     // Remplace l'image et la map en fonction de la cible
     if (target === 'un') {
-        document.getElementById('image').src = "../img/scenes/EntreeDuBatiment.jpeg";
-        document.getElementById('image').useMap = "#workmap";
+        document.getElementById('image').src = "../img/scenes/EntreeDuBatimentFleche.jpeg";
+        document.getElementById('image').useMap = "#EntreeDuBat";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
     } else if (target === 'deux') {
-        document.getElementById('image').src = "../img/scenes/EntreeDuBatiment.jpeg";
-        document.getElementById('image').useMap = "#entreeDuBatMap";
+        document.getElementById('image').src = "../img/scenes/CouloirVuBasFleche.jpeg";
+        document.getElementById('image').useMap = "#Choix";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[2];
     } else if (target === 'trois') {
-        document.getElementById('image').src = "../img/scenes/CouloirVuBas.jpeg";
-        document.getElementById('image').useMap = "#choixMap";
+        document.getElementById('image').src = "../img/scenes/CouloirEtage1Fleche.jpeg";
+        document.getElementById('image').useMap = "#Couloir";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
     } else if (target == 'quatre') {
-        document.getElementById('image').src = "../img/scenes/couloir.jpg";
-        document.getElementById('image').useMap = "#couloir";
+        document.getElementById('image').src = "../img/scenes/Salle.jpg";
+        document.getElementById('image').useMap = "#epreuve1";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
-    } else if (target == 'cinq') {
-        document.getElementById('image').src = "../img/scenes/etage.jpg";
-        document.getElementById('image').useMap = "#etage";
-        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
-    } else if (target == 'cinq') {
-        document.getElementById('image').src = "../img/scenes/etage.jpg";
-        document.getElementById('image').useMap = "#etage";
-        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
-    }
-    else if (target == 'six') {
-        document.getElementById('image').src = "../img/scenes/epreuve1.jpg";
-        document.getElementById('image').useMap = "#epreuve";
-        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
-    }
-    else if (target == 'sept') {
-        document.getElementById('image').src = "../img/scenes/fond.jpg";
-        document.getElementById('image').useMap = "#fond";
-        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
-    } else if (target == 'huit') {
-        document.getElementById('image').src = "../img/scenes/epreuve1.jpg";
+    } 
+     else if (target == 'cinq') {
+        document.getElementById('image').src = "../img/scenes/Salle2.jpg";
         document.getElementById('image').useMap = "#epreuve2";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
     }
+    else if (target == 'six') {
+        document.getElementById('image').src = "../img/scenes/HautDesEscaliersFleche.jpg";
+        document.getElementById('image').useMap = "#Haut";
+        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
+    }
+    else if (target == 'sept') {
+        document.getElementById('image').src = "../img/scenes/CouloirHautFleche.jpg";
+        document.getElementById('image').useMap = "#Etage";
+        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
+    } else if (target == 'huit') {
+        document.getElementById('image').src = "../img/scenes/CouloirHaut2Fleche.jpg";
+        document.getElementById('image').useMap = "#CouloirHaut";
+        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
+    }
     else if (target == 'neuf') {
-        document.getElementById('image').src = "../img/scenes/porte-200D.jpg";
-        document.getElementById('image').useMap = "#porteD";
+        document.getElementById('image').src = "../img/scenes/CouloirSalle200C200G3Fleche.jpg";
+        document.getElementById('image').useMap = "#PorteTP";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
     }
     else if (target == 'dix') {
-        document.getElementById('image').src = "../img/scenes/enfoncement.jpg";
-        document.getElementById('image').useMap = "#enfoncement";
+        document.getElementById('image').src = "../img/Salle3.jpg";
+        document.getElementById('image').useMap = "#Epreuve3";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
     }
     else if (target == 'onze') {
-        document.getElementById('image').src = "../img/scenes/epreuve3.jpg";
-        document.getElementById('image').useMap = "#epreuve3";
+        document.getElementById('image').src = "../img/scenes/Salle5.jpg";
+        document.getElementById('image').useMap = "#Epreuve4";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
     }
     else if (target == 'douze') {
-        document.getElementById('image').src = "../img/scenes/epreuve3.jpg";
-        document.getElementById('image').useMap = "#epreuve4";
-        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
-    }
-    else if (target == 'treize') {
-        document.getElementById('image').src = "../img/scenes/porte-200G.jpg";
-        document.getElementById('image').useMap = "#porteG";
-        //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
-    }
-    else if (target == 'quatorze') {
-        document.getElementById('image').src = "../img/scenes/epreuve3.jpg";
+        document.getElementById('image').src = "../img/scenes/Salle200C.jpg";
         document.getElementById('image').useMap = "#epreuve5";
         //document.getElementById("dialogBoxId").textContent = tableauDialog[1];
     }
